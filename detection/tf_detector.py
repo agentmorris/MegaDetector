542124d7baea3dbbde6e76724b1e83cb6c109eeb
Ștefan Istrate <stefan.istrate@gmail.com>
2023-06-27 09:25:42 +0100

    Add parameter to skip image resizing.

--- detection/tf_detector.py
+++ detection/tf_detector.py
@@ -111,7 +111,7 @@ class TFDetector:
 
         return box_tensor_out, score_tensor_out, class_tensor_out
 
-    def generate_detections_one_image(self, image, image_id, detection_threshold, image_size=None):        
+    def generate_detections_one_image(self, image, image_id, detection_threshold, image_size=None, skip_image_resizing=False):
         """
         Apply the detector to an image.
 
@@ -119,6 +119,7 @@ class TFDetector:
             image: the PIL Image object
             image_id: a path to identify the image; will be in the "file" field of the output object
             detection_threshold: confidence above which to include the detection proposal
+            skip_image_resizing: no-op, added for compatibility with PTDetector
 
         Returns:
         A dict with the following fields, see the 'images' key in https://github.com/agentmorris/MegaDetector/tree/master/api/batch_processing#batch-processing-api-output-format
