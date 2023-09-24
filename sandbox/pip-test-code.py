7331e14bf6bd233538fcf9b35d4442fe06340712
Dan Morris <dmorris@cs.stanford.edu>
2023-09-24 15:56:39 -0700

    pip test code

---
+++ sandbox/pip-test-code.py
@@ -0,0 +1,45 @@
+### Run MegaDetector on one image
+
+from md_utils import url_utils
+from md_visualization import visualization_utils as vis_utils
+from detection import run_detector
+
+# This is the image at the bottom of this page, it has one animal in it
+image_url = 'https://github.com/agentmorris/MegaDetector/raw/main/images/orinoquia-thumb-web.jpg'
+temporary_filename = url_utils.download_url(image_url)
+
+image = vis_utils.load_image(temporary_filename)
+
+# This will automatically download MDv5a to the system temp folder;
+# you can also specify a filename explicitly, or set the $MDV5A
+# environment variable to point to the model file.
+model = run_detector.load_detector('MDV5A')
+
+result = model.generate_detections_one_image(image)
+
+detections_above_threshold = [d for d in result['detections'] if d['conf'] > 0.2]
+print('Found {} detection above threshold'.format(len(detections_above_threshold)))
+
+
+### Run MegaDetector on a folder of images
+
+from detection.run_detector_batch import load_and_run_detector_batch,write_results_to_file
+from md_utils import path_utils
+import os
+
+# Pick a folder to run MD on recursively, and an output file
+image_folder = os.path.expanduser('~/megadetector_test_images')
+output_file = os.path.expanduser('~/megadetector_output_test.json')
+
+# Recursively find images
+image_file_names = path_utils.find_images(image_folder,recursive=True)
+
+# This will automatically download MDv5a to the system temp folder;
+# you can also specify a filename explicitly, or set the $MDV5A
+# environment variable to point to the model file.
+results = load_and_run_detector_batch('MDV5A', image_file_names)
+
+# Write results as relative filenames, this is what Timelapse
+# and other downstream tools expect.
+write_results_to_file(results,output_file,relative_path_base=image_folder)
+
