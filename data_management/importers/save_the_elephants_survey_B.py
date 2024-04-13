3f395a2ea4afbbcfadb4e32c2ac1960ec1a4e78c
Alex Morling <atmorling@gmail.com>
2024-04-13 13:01:49 +0200

    Fix syntax error

--- data_management/importers/save_the_elephants_survey_B.py
+++ data_management/importers/save_the_elephants_survey_B.py
@@ -205,7 +205,7 @@ for image_name in tqdm(list(filenames_to_rows.keys())):
         if (isinstance(species,float) or \
             (isinstance(species,str) and (len(species) == 0))):
             category_name = 'empty'
-        elif species.startswith('?')
+        elif species.startswith('?'):
             category_name = 'unknown'
         else:
             category_name = species
