334656cdcb455050a3a1df7006f427f1c984feab
Dan Morris <dmorris@cs.stanford.edu>
2026-02-15 15:47:33 -0800

    grammar correction in comment

--- megadetector/data_management/read_exif.py
+++ megadetector/data_management/read_exif.py
@@ -744,7 +744,7 @@ def read_exif_from_folder(input_folder,
         list: list of dicts, each of which contains EXIF information for one images.  Fields include at least:
             * 'file_name': the relative path to the image
             * 'exif_tags': a dict of EXIF tags whose exact format depends on [options.processing_library].
-            * 'status' and 'error': only populated when reading EXIF information for an image fails
+            * 'status' and 'error': only populated for images where EXIF reading failed
     """
 
     if options is None:
