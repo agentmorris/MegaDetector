985cabe599ff75b8c11bc8a420bcc9c540012095
Dan Morris <dmorris@cs.stanford.edu>
2026-03-02 10:31:52 -0800

    classification analysis

--- megadetector/postprocessing/analyze_classification_results.py
+++ megadetector/postprocessing/analyze_classification_results.py
@@ -16,6 +16,12 @@ the predictions and the ground truth.
 #%% Imports and constants
 
 
+# See "detection_category_mapping" below
+default_detection_category_mapping = {}
+default_detection_category_mapping['person'] = 'human'
+default_detection_category_mapping['vehicle'] = 'vehicle'
+
+
 #%% Support classes
 
 class ClassificationAnalysisOptions:
@@ -56,8 +62,35 @@ class ClassificationAnalysisOptions:
         #: html_output_dir is not None.
         self.max_images_per_cell = 50
 
+        #: Random seed to be used if image sampling is necessary
+        self.random_seed = 0
+
         #: Confidence threshold to apply to classification (not detection) results
         self.classification_confidence_threshold = 0.5
+
+        #: A dict mapping detection category names to classification category names, for
+        #: categories we want to handle specially.  Any detection in a matching category with
+        #: an above-threshold confidence value is treated as if it had a classification
+        #: with the corresponding (mapped) classification category, with a confidence of 1.0,
+        #: whether or not that category exists in the ground truth.
+        #:
+        #: For example, by default a detection with category "person" with confidence 0.4
+        #: should be treated as a classification of category "human" with confidence 1.0.
+        #:
+        #: Defaults to detection_category_mapping.
+        self.detection_category_mapping = None
+
+        #: If True, the entire analysis will be performed at the *sequence* level, rather
+        #: than the image level.
+        self.sequence_level_analysis = False
+
+        #: Number of workers to use when rendering images
+        self.rendering_workers = 10
+
+        #: Should we use threads ("threads") or processes ("processes") for rendering?
+        #:
+        #: Only relevant if rendering_workers is > 1.
+        self.rendering_pool_type = 'threads'
 
     # ...def __init__(...)
 
@@ -70,6 +103,10 @@ class AnalysisResultOptions:
         #: Dictionary mapping category names to dicts, where each item has
         #: at least the keys "precision", "recall", "n_ground_truth", "n_predicted"
         self.precision_recall_results = None
+
+        self.macro_f1 = None
+        self.micro_f1 = None
+        self.accuracy = None
 
 # ...class AnalysisResultOptions
 
