0d08f8e8200237be0a3411b986304f3e9e198795
Dan Morris <dmorris@cs.stanford.edu>
2026-06-10 15:48:44 -0700

    notebook updates

--- notebooks/compare_inference_options.py
+++ notebooks/compare_inference_options.py
@@ -370,21 +370,23 @@ if open_pages:
         open_file(fn)
 
 
-#%% Scrap
+#%% Create pages for every image
 
-if False:
+# This cell assumes the visualizations have already been generated.
 
-    pass
+import shutil
+from tqdm import tqdm
+from megadetector.utils.write_html_image_list import write_html_image_list
 
-    #%% Review versions of one image
+images_relative = find_images(input_folder,recursive=True, return_relative_paths=True)
+print('Found {} images'.format(len(images)))
 
-    # This cell assumes the visualizations have already been generated.
+image_review_folder = os.path.join(test_folder_base,'image_review')
+os.makedirs(image_review_folder,exist_ok=True)
 
-    import shutil
+image_fn_relative_to_image_index_file = {}
 
-    image_fn_relative = 'IMG_0809.JPG'
-    image_review_folder = os.path.join(test_folder_base,'image_review')
-    os.makedirs(image_review_folder,exist_ok=True)
+for image_fn_relative in tqdm(images_relative):
 
     image_info = []
 
@@ -392,7 +394,11 @@ if False:
     # job_info = all_job_info[0]
     for job_info in all_job_info:
 
-        job_visualization_folder = os.path.join(visualization_folder,job_info['job_name'])
+        image_review_folder_this_image = os.path.join(image_review_folder,
+          image_fn_relative.replace('\\','/').replace('/','_'))
+        os.makedirs(image_review_folder_this_image,exist_ok=True)
+        job_visualization_folder = os.path.join(visualization_folder,
+                                                job_info['job_name'])
         assert os.path.isdir(job_visualization_folder)
 
         # Find the annotated version of this image for this job
@@ -408,7 +414,8 @@ if False:
 
         target_image_relative = os.path.splitext(source_image_relative)[0] + '_' + job_name + \
             os.path.splitext(source_image_relative)[1]
-        target_image_abs = os.path.join(image_review_folder,target_image_relative)
+        target_image_abs = os.path.join(image_review_folder_this_image,
+                                        target_image_relative)
         shutil.copyfile(source_image_abs,target_image_abs)
 
         im = {}
@@ -416,75 +423,143 @@ if False:
         im['filename'] = target_image_relative
         im['title'] = job_name
 
-    # Create an index page
-    from megadetector.utils.write_html_image_list import write_html_image_list
+    # ...for each job
 
+    # Create an index page for this image
     options = write_html_image_list()
-    index_file = os.path.join(image_review_folder,'index.html')
-    write_html_image_list(filename=index_file,
+    index_file_this_image = os.path.join(image_review_folder_this_image,'index.html')
+    write_html_image_list(filename=index_file_this_image,
                           images=image_info,
                           options=options)
+    image_fn_relative_to_image_index_file[image_fn_relative] = \
+        index_file_this_image
 
-    from megadetector.utils.path_utils import open_file
-    open_file(index_file, attempt_to_open_in_wsl_host=True)
+# ...for each image
 
-    #%% Open existing visualizations for specific jobs
+# Generate a master index page
 
-    all_job_names = [x['job_name'] for x in all_job_info]
+master_index_page = os.path.join(image_review_folder,'index.html')
+with open(master_index_page,'w') as f:
+    f.write('<html><body>')
+    for image_fn_relative in image_fn_relative_to_image_index_file:
+        f.write('<a href="{}">{}</a><br/>\n'.format(
+            image_fn_relative_to_image_index_file[image_fn_relative],
+            image_fn_relative
+        ))
+    # ...for each image
+    f.write('</body></html>')
 
-    def find_matching_jobs(html_group_params):
+open_file(master_index_page)
 
-        matching_jobs = []
-        for job_info in all_job_info:
-            job_matches = True
-            for param_name in html_group_params['params']:
-                assert param_name in job_info['params'], \
-                    'No value for {}'.format(param_name)
-                # Empty lists for a parameter means that all jobs match for that
-                # parameter
-                if (html_group_params['params'][param_name] is None) or \
-                   (len(html_group_params['params'][param_name]) == 0):
-                    continue
-                if job_info['params'][param_name] not in html_group_params['params'][param_name]:
-                    job_matches = False
-                    break
-            if job_matches:
-                matching_jobs.append(job_info)
-        return matching_jobs
 
-    html_groups = []
+#%% Review versions of one image
 
+# This cell assumes the visualizations have already been generated.
+
+import shutil
+
+image_fn_relative = 'IMG_0809.JPG'
+image_review_folder = os.path.join(test_folder_base,'image_review')
+os.makedirs(image_review_folder,exist_ok=True)
+
+image_info = []
+
+# For each job
+# job_info = all_job_info[0]
+for job_info in all_job_info:
+
+    job_visualization_folder = os.path.join(visualization_folder,job_info['job_name'])
+    assert os.path.isdir(job_visualization_folder)
+
+    # Find the annotated version of this image for this job
+    annotated_images = os.listdir(job_visualization_folder)
+    source_image = [fn for fn in annotated_images if image_fn_relative in fn]
+    assert len(source_image) == 1
+    source_image_relative = source_image[0]
+    source_image_abs = os.path.join(job_visualization_folder,source_image_relative)
+    assert os.path.isfile(source_image_abs)
+
+    # Copy it to the output folder, with a filename that includes the job name
+    job_name = job_info['job_name']
+
+    target_image_relative = os.path.splitext(source_image_relative)[0] + '_' + job_name + \
+        os.path.splitext(source_image_relative)[1]
+    target_image_abs = os.path.join(image_review_folder,target_image_relative)
+    shutil.copyfile(source_image_abs,target_image_abs)
+
+    im = {}
+    image_info.append(im)
+    im['filename'] = target_image_relative
+    im['title'] = job_name
+
+# Create an index page
+from megadetector.utils.write_html_image_list import write_html_image_list
+
+options = write_html_image_list()
+index_file = os.path.join(image_review_folder,'index.html')
+write_html_image_list(filename=index_file,
+                        images=image_info,
+                        options=options)
+
+from megadetector.utils.path_utils import open_file
+open_file(index_file, attempt_to_open_in_wsl_host=True)
+
+
+#%% Open existing visualizations for specific jobs
+
+all_job_names = [x['job_name'] for x in all_job_info]
+
+def find_matching_jobs(html_group_params):
+
+    matching_jobs = []
+    for job_info in all_job_info:
+        job_matches = True
+        for param_name in html_group_params['params']:
+            assert param_name in job_info['params'], \
+                'No value for {}'.format(param_name)
+            # Empty lists for a parameter means that all jobs match for that
+            # parameter
+            if (html_group_params['params'][param_name] is None) or \
+                (len(html_group_params['params'][param_name]) == 0):
+                continue
+            if job_info['params'][param_name] not in html_group_params['params'][param_name]:
+                job_matches = False
+                break
+        if job_matches:
+            matching_jobs.append(job_info)
+    return matching_jobs
+
+html_groups = []
+
+html_group = {}
+html_group['name'] = 'test'
+html_group['params'] = {}
+html_group['params']['model_name'] = ['mdv5a']
+html_group['params']['image_size'] = None # [1600]
+html_group['params']['compatibility_mode'] = None # ['modern']
+html_group['params']['aug'] = None # ['noaug','aug']
+html_group['params']['tiling'] = ['tiling']
+html_group['matching_jobs'] = find_matching_jobs(html_group)
+
+# Choose a specific subset of jobs by name
+if False:
     html_group = {}
-    html_group['name'] = 'test'
-    html_group['params'] = {}
-    html_group['params']['model_name'] = ['mdv5a']
-    html_group['params']['image_size'] = None # [1600]
-    html_group['params']['compatibility_mode'] = None # ['modern']
-    html_group['params']['aug'] = None # ['noaug','aug']
-    html_group['params']['tiling'] = ['tiling']
-    html_group['matching_jobs'] = find_matching_jobs(html_group)
-
-    # Choose a specific subset of jobs by name
-    if False:
-        html_group = {}
-        html_group['name'] = 'no-tiling-best'
-        html_group['matching_names'] = [
-            'aug_modern_none_mdv1000-redwood_no-tiling',
-            'aug_modern_none_mdv5a_no-tiling',
-            'aug_classic_none_mdv1000-redwood_no-tiling',
-            'aug_classic_1600_mdv1000-redwood_no-tiling',
-            'noaug_modern_none_mdv1000-redwood_no-tiling'
-        ]
-        html_group['matching_jobs'] = [x for x in all_job_info if x['job_name'] in html_group['matching_names']]
-        assert len(html_group['matching_jobs']) == len(html_group['matching_names'])
+    html_group['name'] = 'no-tiling-best'
+    html_group['matching_names'] = [
+        'aug_modern_none_mdv1000-redwood_no-tiling',
+        'aug_modern_none_mdv5a_no-tiling',
+        'aug_classic_none_mdv1000-redwood_no-tiling',
+        'aug_classic_1600_mdv1000-redwood_no-tiling',
+        'noaug_modern_none_mdv1000-redwood_no-tiling'
+    ]
+    html_group['matching_jobs'] = [x for x in all_job_info if x['job_name'] in html_group['matching_names']]
+    assert len(html_group['matching_jobs']) == len(html_group['matching_names'])
 
 
-    print('Found {} matching jobs for {}'.format(
-        len(html_group['matching_jobs']),
-        html_group['name']))
+print('Found {} matching jobs for {}'.format(
+    len(html_group['matching_jobs']),
+    html_group['name']))
 
-    #%%
-
-    for matching_job in html_group['matching_jobs']:
-        open_file(matching_job['html_output_file'],attempt_to_open_in_wsl_host=True)
+for matching_job in html_group['matching_jobs']:
+    open_file(matching_job['html_output_file'],attempt_to_open_in_wsl_host=True)
 
